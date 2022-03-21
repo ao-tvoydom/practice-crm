11dff0b9ff44dbbe0446fdca78c6d6f5d8f5f341
Timeset <drakozavr.hy@mail.ru>
2022-03-21 13:37:38 +0300

    Product Controller

--- ProductCRM/ProductCRM/Program.cs
+++ ProductCRM/ProductCRM/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Migrations.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CRMDbContext>();
+
+
 var app = builder.Build();
 
+
+
+app.MapControllerRoute(name: "DefaultApi",
+    pattern: "api/{controller}/{id}"
+    );
+
+app.MapControllers();
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
