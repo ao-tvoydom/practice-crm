615a46011aa9bf579c9c02e00129ef148b64a5fd
Timeset <drakozavr.hy@mail.ru>
2022-03-25 10:40:56 +0300

    API Documentation

--- ProductCRM/ProductCRM/Program.cs
+++ ProductCRM/ProductCRM/Program.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Migrations.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CRMDbContext>();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() {Title = "CRM API", Version = "v1"});
+});
 
 var app = builder.Build();
 
 
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("v1/swagger.json", "Api V1");
+});
 app.MapControllerRoute(name: "DefaultApi",
     pattern: "api/{controller}/{id}"
     );
