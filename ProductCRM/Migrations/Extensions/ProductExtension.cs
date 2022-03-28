1a2dd068f8c34faf98234d15b9e2ebf71a4c4042
Timeset <drakozavr.hy@mail.ru>
2022-03-28 15:22:07 +0300

    Ignore null CategoryIdArray on GET

--- ProductCRM/Migrations/Extensions/ProductExtension.cs
+++ ProductCRM/Migrations/Extensions/ProductExtension.cs
@@ -2,11 +2,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace Migrations.Entities;
+ namespace Migrations.Entities;
 
 
-public partial class Product
-{
-    public int[]? CategoryIdArray { get;  set; }
-    
-}
+ public partial class Product
+ {
+     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+     public int[]? CategoryIdArray { get;  set; }
+     
+ }
